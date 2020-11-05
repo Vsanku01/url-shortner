320907ccef544c1b4acf5cdf7b92c9dca05ce9d5
Vsanku01 <ethhack001@gmail.com>
2020-11-05 19:28:26 +0530

    :deployed:

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/url');
+require('dotenv').config();
 const app = express();
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.static('./public'));
 
-const port = process.env.PORT || 1337;
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  function (err) {
+    if (err) return console.log('Error: ', err);
+    console.log(
+      'MongoDB Connection -- Ready state is:',
+      mongoose.connection.readyState
+    );
+  }
+);
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
 });
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
